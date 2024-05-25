ddd17a16a73725af642c82e5946d5adbe17e6ff7
Sanml222 <Santiagoml222@gmail.com>
2024-05-25 15:28:48 -0500

    ClienteMemento

--- proyecto_yamaha/src/main/java/co/edu/uniquindio/proyecto/yamaha/proyecto_yamaha/model/Cliente.java
+++ proyecto_yamaha/src/main/java/co/edu/uniquindio/proyecto/yamaha/proyecto_yamaha/model/Cliente.java
@@ -1,7 +1,7 @@
 package co.edu.uniquindio.proyecto.yamaha.proyecto_yamaha.model;
 
 import co.edu.uniquindio.proyecto.yamaha.proyecto_yamaha.model.builder.ClienteBuilder;
-
+import co.edu.uniquindio.proyecto.yamaha.proyecto_yamaha.model.memento.ClienteMemento;
 
 public class Cliente extends Persona {
 
@@ -11,6 +11,22 @@ public class Cliente extends Persona {
     }
     public static ClienteBuilder builder(){
         return new ClienteBuilder();
+    }
+
+
+    public ClienteMemento guardarMemento(){
+        return new ClienteMemento(this.getNombre(), this.getCedula(), this.getEmail(), this.getCelular(), this.getEdad());
+    }
+
+    public void restaurarDesdeMemento(ClienteMemento clienteMemento)
+    {
+        Cliente.builder()
+                .nombre(clienteMemento.getNombre())
+                .cedula(clienteMemento.getCedula())
+                .email(clienteMemento.getEmail())
+                .celular(clienteMemento.getCelular())
+                .edad(clienteMemento.getEdad())
+                .build();
     }
 
 
